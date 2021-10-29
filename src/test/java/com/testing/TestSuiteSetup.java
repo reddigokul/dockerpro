eafeca11636a69afea5da43b36563e54efd1a643
reddigokul <gokulsetty@gmail.com>
2021-10-29 17:08:11 +0530

    TestSuiteSetup updated

--- src/test/java/com/testing/TestSuiteSetup.java
+++ src/test/java/com/testing/TestSuiteSetup.java
@@ -1,63 +1,75 @@
 package com.testing;
 
-import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TestSuiteSetup {
+	
 	WebDriver driver;
+	public String baseUrl = "https://www.google.com/";
+	String driverPath = System.getProperty("user.dir")+"/drivers/chromedriver.exe";
+	
 
 	@BeforeSuite(alwaysRun = true)
 	public void setupSuite() {
-		driver = new FirefoxDriver();
+		System.out.println("This is BeforeSuite annotation method  in TestSuiteSetup Class");
+
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
 	}
 
 	@AfterSuite(alwaysRun = true)
 	public void tearDown() {
+		System.out.println("This is AfterSuite annotation method  in TestSuiteSetup Class");
+
 		driver.close();
 	}
 
-	@BeforeClass(description = "Set capabilities for your Firefox browser and set time it should wait for a page to load.")
-	public void firefoxSetUp() throws MalformedURLException {
+	@BeforeClass(description = "Set capabilities for your Chrome browser and set time it should wait for a page to load.")
+	public void chromeSetUp(){
+		//System.setProperty("webdriver.chrome.driver", driverPath);
+		//driver = new ChromeDriver();
+		System.out.println("This is BeforeClass annotation method  in TestSuiteSetup Class");
 
-		// DesiredCapabilities capability = new DesiredCapabilities..firefox();
-		driver = new ChromeDriver();
-		// driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.manage().window().setSize(new Dimension(1920, 1080));
+		driver.navigate().to("http://facebook.com");
 
 	}
 
-	@AfterClass(description = "close your firefox driver")
+	@AfterClass(description = "close your chrome driver")
 	public void afterclass() {
+		System.out.println("This is AfterClass  annotation method  in TestSuiteSetup Class");
+
 		driver.close();
 	}
 
-	@BeforeTest(description = "set your firefox profile preferences according to your project requirement")
+	@BeforeTest(description = "set your chrome profile preferences according to your project requirement")
 	public void single_run() {
-		FirefoxProfile firefoxProfile = new FirefoxProfile();
-		firefoxProfile.setPreference("browser.download.folderList", 2);
-		firefoxProfile.setPreference("browser.download.manager.showWhenStarting", false);
-		firefoxProfile.setPreference("browser.download.dir", "E:\\reports\\");
-		firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk", "csv");
-		driver = new FirefoxDriver();
-		String baseUrl = "www.gmail.com";
+		//driver = new ChromeDriver();
+		System.out.println("This is BeforeTest annotation method  in TestSuiteSetup Class");
+
+		String url1 = "http://www.gmail.com";
+		driver.get(url1);
 	}
+	
+	@Test
+	public void testCase1() {
+		System.out.println("This is my First Test Case in TestSuiteSetup Class");
+	}
+
 
 	@AfterTest(description = "")
-	public void teardown() {
-		// a code which will send the test details report
+	public void closeDriver() {
+		System.out.println("This is AfterTest annotation method  in TestSuiteSetup Class");
+		//driver.close();
 	}
 
 }
